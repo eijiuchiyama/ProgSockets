4fef7349f19e5f685f43df77c096b0d11a515187
Felipe Kelemen Bernardino <felipe.kelemen02@gmail.com>
2026-04-13 19:21:11 -0300

    Refactor handler and protocol code; remove unused files and improve serialization logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,120 +1,118 @@
+#include "handler.hpp"
+#include "parser.hpp"
+#include "protocol.hpp"
+#include "resource_man.hpp"
+#include <fstream>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <string>
-#include <semaphore.h>
-#include <fstream>
-#include "resource_man.hpp"
-#include "parser.hpp"
-#include "protocol.hpp"
-#include "handler.hpp"
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 sem_t mutex;
 
-std::string serialize(ParseResult parse_result, Response response, std::ofstream &file, pthread_t self)
-{
+std::string serialize(ParseResult parse_result, Response response,
+                      std::ofstream &file, pthread_t self) {
 
-	std::string error_codes[8] = {"", "Recurso já reservado", "Recurso inexistente", "Recurso não reservado", "", "Limite de recursos", "", "Método inexistente"};
-
-	if(response.status_code != 0){
-		file << "ERRO " << std::to_string(response.status_code) << " " << error_codes[response.status_code] << std::endl;
-		return std::string(u8"😡​") + " " + std::to_string(response.status_code) + " " + error_codes[response.status_code] + "\n";
-	} else if(parse_result.msg.command == GET){
-		return std::string(u8"👍") + " " + response.value + "\n";
-	} else if(parse_result.msg.command == SET){
-		return std::string(u8"👍") + "\n";
-	} else if(parse_result.msg.command == CREATE){
-		return std::string(u8"👍") + " " + std::to_string(response.id) + "\n";
-	} else if(parse_result.msg.command == RESERVE){
-		return std::string(u8"👍") + "\n";
-	} else if(parse_result.msg.command == RELEASE){
-		return std::string(u8"👍") + "\n";
-	} else if(parse_result.msg.command == LIST){
-		std::string s = "";
-		s += std::string(u8"👍");
-		s += " "; 
-		s += std::to_string(response.count);
-		for(int i = 0; i < response.count; i++){
-			s += " ";
-			s += std::to_string(response.list[i]->id);
-		}
-		s += "\n";
-		return s;
-	}
-}
-
-void *handler(void *args)
-{
-
-	int conn = *((int *)args);
-	free(args);
-	char received_message[MAX_MESSAGE + 1];
-	pthread_t self = pthread_self();
-	long n;
-	ParseResult parse_result;
-	Response response;
-	std::string serial_response;
-	
-	std::ofstream file("log.txt", std::ios::app);
-	file << "CONEXAO " << self << std::endl;
-  
-    while ((n = read(conn, received_message, MAX_MESSAGE)) > 0)
-    {
-      received_message[n] = 0;
-      printf("[Cliente enviou:] %s", received_message);
-      file << received_message << " " << self << std::endl;
-
-      parse_result = parse(received_message); 
-      response = return_response(parse_result, &self);
-      
-      serial_response = serialize(parse_result, response, file, self);
-      write(conn, serial_response.c_str(), serial_response.length());
+  if (response.status_code != 0) {
+    file << "ERRO " << std::to_string(response.status_code) << " "
+         << error_codes[response.status_code] << std::endl;
+    return std::string(ERROR_CODE) + " " +
+           std::to_string(response.status_code) + " " +
+           error_codes[response.status_code] + "\n";
+  } else if (parse_result.msg.command == GET) {
+    return std::string(OK_CODE) + " " + response.value + "\n";
+  } else if (parse_result.msg.command == SET) {
+    return std::string(OK_CODE) + "\n";
+  } else if (parse_result.msg.command == CREATE) {
+    return std::string(OK_CODE) + " " + std::to_string(response.id) + "\n";
+  } else if (parse_result.msg.command == RESERVE) {
+    return std::string(OK_CODE) + "\n";
+  } else if (parse_result.msg.command == RELEASE) {
+    return std::string(OK_CODE) + "\n";
+  } else if (parse_result.msg.command == LIST) {
+    std::string s = "";
+    s += std::string(OK_CODE);
+    s += " ";
+    s += std::to_string(response.count);
+    for (int i = 0; i < response.count; i++) {
+      s += " ";
+      s += std::to_string(response.list[i]->id);
     }
-  
-    printf("[Uma conexão encerrada]\n");
-    file << "DESCONEXAO " << self << std::endl;
-    file.close();
-    release_all_from_client(&self);
-
-	return NULL;
+    s += "\n";
+    return s;
+  } else {
+    return std::string(ERROR_CODE) + " " + std::to_string(UNKNOWN_COMMAND) +
+           " " + error_codes[UNKNOWN_COMMAND] + "\n";
+  }
 }
 
-int main(int argc, char **argv)
-{
+void *handler(void *args) {
+
+  int conn = *((int *)args);
+  free(args);
+  char received_message[MAX_MESSAGE + 1];
+  pthread_t self = pthread_self();
+  long n;
+  ParseResult parse_result;
+  Response response;
+  std::string serial_response;
+
+  std::ofstream file("log.txt", std::ios::app);
+  file << "CONEXAO " << self << std::endl;
+
+  while ((n = read(conn, received_message, MAX_MESSAGE)) > 0) {
+    received_message[n] = 0;
+    printf("[Cliente enviou:] %s", received_message);
+    file << received_message << " " << self << std::endl;
+
+    parse_result = parse(received_message);
+    response = return_response(parse_result, &self);
+
+    serial_response = serialize(parse_result, response, file, self);
+    write(conn, serial_response.c_str(), serial_response.length());
+  }
+
+  printf("[Uma conexão encerrada]\n");
+  file << "DESCONEXAO " << self << std::endl;
+  file.close();
+  release_all_from_client(&self);
+
+  return NULL;
+}
+
+int main(int argc, char **argv) {
   int listen_socket, connection_socket;
   struct sockaddr_in server_info;
 
-  if ((listen_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-  {
+  if ((listen_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
     perror("erro ao criar socket\n");
     exit(2);
   }
 
   int opt = 1;
-  if (setsockopt(listen_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(int)) < 0)
-  {
+  if (setsockopt(listen_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(int)) <
+      0) {
     perror("setsockopt(SO_REUSEADDR) failed");
   }
-
 
   bzero(&server_info, sizeof(server_info));
   server_info.sin_family = AF_INET;
   server_info.sin_addr.s_addr = htonl(INADDR_ANY);
   int port = atoi(argv[1]);
   server_info.sin_port = htons(port);
-  if ((bind(listen_socket, (struct sockaddr *)&server_info, sizeof(server_info)) == -1))
-  {
+  if ((bind(listen_socket, (struct sockaddr *)&server_info,
+            sizeof(server_info)) == -1)) {
     perror("erro no bind\n");
     exit(3);
   }
 
-  if (listen(listen_socket, 1) == -1)
-  {
+  if (listen(listen_socket, 1) == -1) {
     perror("erro no listen\n");
     exit(4);
   }
@@ -122,18 +120,17 @@ int main(int argc, char **argv)
   printf("[Servidor no ar. Aguardando conexões na porta %s]\n", argv[1]);
 
   sem_init(&mutex, 0, 1);
-  
-  for (;;)
-  {
-    if ((connection_socket = accept(listen_socket, (struct sockaddr *)NULL, NULL)) == -1)
-    {
+
+  for (;;) {
+    if ((connection_socket =
+             accept(listen_socket, (struct sockaddr *)NULL, NULL)) == -1) {
       perror("erro no accept\n");
       exit(5);
     };
 
     pthread_t thread;
 
-    int *conn_copy = (int*) malloc(sizeof(int));
+    int *conn_copy = (int *)malloc(sizeof(int));
     *conn_copy = connection_socket;
     pthread_create(&thread, NULL, handler, conn_copy);
     pthread_detach(thread);
--- src/protocol.hpp
+++ src/protocol.hpp
@@ -1,22 +1,23 @@
 #pragma once
 
 #include <pthread.h>
+#include <string>
 
 #define MAX_MESSAGE 4096
 
 typedef enum {
-    CREATE,
-    GET,
-    SET,
-    LIST,
-    RESERVE,
-    RELEASE,
+  CREATE,
+  GET,
+  SET,
+  LIST,
+  RESERVE,
+  RELEASE,
 } Command;
 
 typedef struct {
   Command command;
   long id;
-  char* value;
+  char *value;
 } Message;
 
 typedef enum {
@@ -28,3 +29,15 @@ typedef struct {
   Message msg;
   ParseError error;
 } ParseResult;
+
+const std::string OK_CODE = u8"👍";
+const std::string ERROR_CODE = u8"😡";
+
+const std::string error_codes[8] = {"",
+                                    "Recurso já reservado",
+                                    "Recurso inexistente",
+                                    "Recurso não reservado",
+                                    "",
+                                    "Limite de recursos",
+                                    "",
+                                    "Método inexistente"};
